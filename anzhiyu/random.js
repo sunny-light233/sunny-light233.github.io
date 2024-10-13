4f6c00b0806e5487f19f9f39e5c59b53b36df72b
sunny-light233 <1968878003@qq.com>
2024-10-13 16:35:08 +0800

    Site updated: 2024-10-13 16:35:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/12/HTML元信息/","2024/10/12/HTML文本处理/","2024/10/12/hello-world/","2024/10/12/HTML/","2024/10/12/HTML简单概述/"];function toRandomPost(){
+var posts=["2024/10/12/HTML元信息/","2024/10/12/hello-world/","2024/10/12/HTML文本处理/","2024/10/12/HTML简单概述/","2024/10/12/HTML/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
